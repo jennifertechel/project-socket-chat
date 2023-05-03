6fe7b4a7590603dc75d8d0383a7b08e31a8aac54
Martina Kreivi <martinakreivi@gmail.com>
2023-05-03 13:15:53 +0200

    Added logo and text, button, placeholder

--- client/src/pages/StartPage.tsx
+++ client/src/pages/StartPage.tsx
@@ -1,11 +1,22 @@
-import { Box, Image, Input } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Input, Text } from "@chakra-ui/react";
 
 function StartPage() {
   return (
-    <Box boxSize="sm">
-      <Image src="src/assets/logoWithStamp.png" />
-      <Input placeholder="Nickname" variant="flushed" width="auto"></Input>
-    </Box>
+    <Flex>
+      <Box
+        boxSize="sm"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Image src="src/assets/CHATROPOLIS.svg" />
+        <Image src="src/assets/logoWithStamp.png" />
+        <Text>Enter nickname</Text>
+        <Input placeholder="Nickname" variant="flushed" width="auto"></Input>
+        <Button>Let's Chat!</Button>
+      </Box>
+    </Flex>
   );
 }
 
