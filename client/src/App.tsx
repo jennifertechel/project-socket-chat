d70f19d58f7eaef8a46cc8d8f22355eeff61a176
Jennifer Techel <jennifertechel@hotmail.com>
2023-05-03 11:16:47 +0200

    add routing for pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <h1>HELLO MOTHERFUCKERS</h1>;
+  return <Outlet />;
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,6 +10,35 @@ import type {
 } from "../../server/src/communication";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import StartPage from "./pages/StartPage";
+import HomePage from "./pages/HomePage";
+import RoomHomePage from "./pages/RoomHomePage";
+import PrivateHomePage from "./pages/PrivateHomePage";
+import RoomNewPage from "./pages/RoomNewPage";
+import RoomChat from "./pages/RoomChat";
+import PrivateNewPage from "./pages/PrivateNewPage";
+import PrivateChat from "./pages/PrivateChat";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index element={<StartPage />} />
+      <Route path='home' element={<HomePage />} />
+      <Route path='room' element={<RoomHomePage />} />
+      <Route path='room/new' element={<RoomNewPage />} />
+      <Route path='room/chat' element={<RoomChat />} />
+      <Route path='private' element={<PrivateHomePage />} />
+      <Route path='private/new' element={<PrivateNewPage />} />
+      <Route path='private/chat' element={<PrivateChat />} />
+    </Route>
+  )
+);
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
 
@@ -17,7 +46,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <SocketProvider>
       <ChakraProvider theme={theme}>
-        <App />
+        <RouterProvider router={router} />
       </ChakraProvider>
     </SocketProvider>
   </React.StrictMode>
