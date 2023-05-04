0b907834c8907111f052475093ab2e4f82085f0c
Martina Kreivi <martinakreivi@gmail.com>
2023-05-04 13:07:08 +0200

    (wip)

--- client/src/pages/RoomHomePage.tsx
+++ client/src/pages/RoomHomePage.tsx
@@ -1,5 +1,92 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { useLocation, useNavigate } from "react-router-dom";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+// responsivitet i theme? change props : props
+
 function RoomHomePage() {
-  return <h1>This is room home page</h1>;
+  const location = useLocation();
+  //const nickname = location.state.nickname;
+  const navigate = useNavigate();
+
+  function handleStartChat() {
+    //socket.emit("join", nickname);
+    navigate("/home");
+  }
+  function handleNewRoom() {
+    //socket.emit("join", nickname);
+    navigate("/room/new");
+  }
+  function handleJoinRoom() {
+    //socket.emit("join", nickname);
+    navigate("/room");
+  }
+
+  const boxSize = useBreakpointValue({
+    base: "sm",
+    md: "md",
+    lg: "lg",
+    xl: "2xl",
+  });
+
+  return (
+    <Flex justifyContent="center" alignItems="center">
+      <Box
+        boxSize="200px"
+        mt="100px"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Image src="src/assets/CHATROPOLIS.svg" />
+        <Image src="src/assets/logoWithStamp.png" />
+
+        <Text m="30px">Welcome !</Text>
+        <Flex flexDirection="row" gap="10px">
+          <Button mt="20px" onClick={handleNewRoom}>
+            New Room
+          </Button>
+          <Button mt="20px" onClick={handleJoinRoom}>
+            Join Room
+          </Button>
+        </Flex>
+        <Flex flexDirection="column">
+          <Box
+            border="1px solid"
+            borderColor="#6B6262"
+            background="#f5f5f5"
+            m="10px"
+            p="1px 10px"
+            w={boxSize}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Text textAlign="left">Paris</Text>
+            <Text textAlign="right">5 Online</Text>
+          </Box>
+          <Box
+            border="1px solid"
+            borderColor="#6B6262"
+            background="#f5f5f5"
+            m="10px"
+            p="1px 10px"
+            w={boxSize}
+          >
+            hej
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
+  );
 }
 
 export default RoomHomePage;
