d4fe674a36b754ec155e19780901dc4b68480331
JennyWeij <jennyweij@outlook.com>
2023-05-04 14:22:17 +0200

    lagt till chattruta, form, knapp

--- client/src/pages/RoomChat.tsx
+++ client/src/pages/RoomChat.tsx
@@ -1,5 +1,28 @@
+import { Box, Image, Text } from "@chakra-ui/react";
+import ChatMessage from "../components/ChatMessage";
+
 function RoomChat() {
-  return <h1>This is room chat</h1>;
+  return (
+    <>
+      <Box h="80px">
+        <Image src="src/assets/CHATROPOLIS.svg" />
+      </Box>
+
+      <Box
+        bg="#D14F9D"
+        w="345px"
+        h="50px"
+        ml="0.5rem"
+        border="1px"
+        textAlign="center"
+      >
+        <Text fontFamily="Montserrat" color="white">
+          Tips in Paris
+        </Text>
+      </Box>
+      <ChatMessage />
+    </>
+  );
 }
 
 export default RoomChat;
