cf2b96a6d11db42d48ded0540fa8c11ccfa3ad15
Jennifer Techel <jennifertechel@hotmail.com>
2023-05-06 18:23:41 +0200

    rename function

--- client/src/pages/RoomHomePage.tsx
+++ client/src/pages/RoomHomePage.tsx
@@ -9,14 +9,19 @@ import {
 import { useEffect, useState } from "react";
 import ActiveRooms from "../components/ActiveRooms";
 import { useSocket } from "../context/SocketContext";
+import { useNavigate } from "react-router-dom";
 
 function RoomHomePage() {
   const { socket, nickname, setNickname } = useSocket();
-  const [showJoinRoom, setShowJoinRoom] = useState(false);
+  const [showRooms, setShowRooms] = useState(false);
+  const navigate = useNavigate();
 
-  const handleJoinRoomClick = () => {
-    // Toggle the value of showNewElement
-    setShowJoinRoom((prevState) => !prevState);
+  const toggleShowRooms = () => {
+    setShowRooms((prevState) => !prevState);
+  };
+
+  const handleNewRoom = () => {
+    navigate("/room/new");
   };
 
   useEffect(() => {
@@ -38,28 +43,30 @@ function RoomHomePage() {
   });
 
   return (
-    <Flex justifyContent="center" alignItems="center" flexDir="column">
+    <Flex justifyContent='center' alignItems='center' flexDir='column'>
       <Box
         boxSize={boxSize}
-        mt="100px"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
+        mt='100px'
+        display='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'
       >
-        <Image src="src/assets/CHATROPOLIS.svg" />
-        <Image src="src/assets/logoWithStamp.png" />
+        <Image src='src/assets/CHATROPOLIS.svg' />
+        <Image src='src/assets/logoWithStamp.png' />
 
-        <Text m="30px">Welcome {nickname}!</Text>
-        <Flex flexDirection="row" gap="10px">
-          <Button mt="20px">New Room</Button>
-          <Button mt="20px" onClick={handleJoinRoomClick}>
+        <Text m='30px'>Welcome {nickname}!</Text>
+        <Flex flexDirection='row' gap='10px'>
+          <Button mt='20px' onClick={handleNewRoom}>
+            New Room
+          </Button>
+          <Button mt='20px' onClick={toggleShowRooms}>
             Join Room
           </Button>
         </Flex>
       </Box>
 
-      {showJoinRoom && <ActiveRooms />}
+      {showRooms && <ActiveRooms />}
     </Flex>
   );
 }
