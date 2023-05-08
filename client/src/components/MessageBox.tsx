e875ebe264b82a6d761599446822458c88ee25b5
Jennifer Techel <jennifertechel@hotmail.com>
2023-05-08 16:47:52 +0200

    placed ul outside of <Text>

--- client/src/components/MessageBox.tsx
+++ client/src/components/MessageBox.tsx
@@ -9,37 +9,31 @@ export default function MessageBox() {
   return (
     <>
       <Flex
-        bg="#F5F5F5"
-        mt="1rem"
-        w="345px"
-        ml="0.5rem"
-        align="center"
-        alignItems="flex-start"
+        bg='#F5F5F5'
+        mt='1rem'
+        w='345px'
+        ml='0.5rem'
+        align='center'
+        alignItems='flex-start'
       >
-        <Avatar bg="teal.500" />
-        <Box bg="#F5F5F5">
+        <Avatar bg='brand.700' />
+        <Box bg='#F5F5F5'>
           <Text
-            fontFamily="Montserrat"
-            fontSize="0.8rem"
-            ml="0.5rem"
-            mt="0.3rem"
-            fontWeight="bold"
-            color="#6B6262"
+            fontFamily='Montserrat'
+            fontSize='0.8rem'
+            ml='0.5rem'
+            mt='0.3rem'
+            fontWeight='bold'
+            color='#6B6262'
           >
             {nickname}
           </Text>
-          <Text
-            fontFamily="Montserrat"
-            ml="0.5rem"
-            fontSize="0.8rem"
-            color="#6B6262"
-          >
-            <ul style={{ listStyleType: "none" }}>
-              {messages.map((message, i) => (
-                <li key={i}>{message.message}</li>
-              ))}
-            </ul>
-          </Text>
+
+          <ul style={{ listStyleType: "none" }}>
+            {messages.map((message, i) => (
+              <li key={i}>{message.message}</li>
+            ))}
+          </ul>
         </Box>
       </Flex>
 
