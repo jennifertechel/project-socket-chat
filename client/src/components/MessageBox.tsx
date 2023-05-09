6b0de81de0f86f3b67094ec74626c309427be651
JennyWeij <jennyweij@outlook.com>
2023-05-09 11:20:45 +0200

    nu renderas avataren högst upp

--- client/src/components/MessageBox.tsx
+++ client/src/components/MessageBox.tsx
@@ -20,23 +20,23 @@ export default function MessageBox() {
           {messages.map((message, i) => (
             <li key={i}>
               <Flex alignItems="center">
-                <Avatar bg="brand.700" />
+                <Avatar bg="brand.700" alignSelf="flex-start" />
                 <Box ml="0.5rem" bg="#F5F5F5">
                   <Flex flexDirection="column">
                     <Text
                       fontFamily="Montserrat"
                       fontSize="0.8rem"
                       fontWeight="bold"
-                      mt="0.5rem"
+                      // mt="0.5rem"
                       color="#6B6262"
                     >
                       {message.nickname}
                     </Text>
                     <Text>{message.message}</Text>
-                    <Box h="1rem" color="white"></Box>
                   </Flex>
                 </Box>
               </Flex>
+              <Box h="0.5rem" w="345px" bgColor="white"></Box>
             </li>
           ))}
         </ul>
