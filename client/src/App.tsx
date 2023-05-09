0cd66e13698160fff6e8e639e5b4acdcde505aed
Jennifer Techel <jennifertechel@hotmail.com>
2023-05-09 13:42:22 +0200

    Merge branch 'main' into 41-fix-spacing-room-list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,6 @@
-import { Flex, useMediaQuery } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  const [isMobile] = useMediaQuery("(max-width: 768px)");
-
   return <Outlet />;
 }
 
