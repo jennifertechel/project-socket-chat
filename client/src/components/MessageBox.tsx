4cf0c5e9f507df6e1091d4a190e517e0ffc42108
JennyWeij <jennyweij@outlook.com>
2023-05-09 11:30:04 +0200

    skapat mellanrum mellan meddelandena

--- client/src/components/MessageBox.tsx
+++ client/src/components/MessageBox.tsx
@@ -9,7 +9,6 @@ export default function MessageBox() {
   return (
     <>
       <Flex
-        bg="#F5F5F5"
         mb="1rem"
         w="345px"
         ml="0.5rem"
@@ -19,15 +18,16 @@ export default function MessageBox() {
         <ul style={{ listStyleType: "none" }}>
           {messages.map((message, i) => (
             <li key={i}>
-              <Flex alignItems="center">
-                <Avatar bg="brand.700" alignSelf="flex-start" />
-                <Box ml="0.5rem" bg="#F5F5F5">
+              <Flex alignItems="center" bg="#F5F5F5" mb={2}>
+                <Box p={2} alignSelf="flex-start">
+                  <Avatar bg="brand.700" size="md" />
+                </Box>
+                <Box ml="0.5rem">
                   <Flex flexDirection="column">
                     <Text
                       fontFamily="Montserrat"
                       fontSize="0.8rem"
                       fontWeight="bold"
-                      // mt="0.5rem"
                       color="#6B6262"
                     >
                       {message.nickname}
@@ -36,7 +36,6 @@ export default function MessageBox() {
                   </Flex>
                 </Box>
               </Flex>
-              <Box h="0.5rem" w="345px" bgColor="white"></Box>
             </li>
           ))}
         </ul>
