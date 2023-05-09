613265274e2f457a71096e44fe1287a9509cbc3f
Martina Kreivi <martinakreivi@gmail.com>
2023-05-09 12:54:50 +0200

    Link to home in header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,31 +4,33 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <Flex
-      pos='fixed'
+      pos="fixed"
       top={0}
-      w='100vw'
-      justifyContent='space-evenly'
-      alignItems='center'
+      w="100vw"
+      justifyContent="space-evenly"
+      alignItems="center"
       h={20}
-      borderBottom='1px solid lightgrey'
+      borderBottom="1px solid lightgrey"
     >
-      <Heading
-        fontFamily='Luckiest Guy'
-        color='brand.900'
-        fontSize={46}
-        mr='500px'
-        mt={4}
-      >
-        Chatropolis
-      </Heading>
+      <Link to="/home">
+        <Heading
+          fontFamily="Luckiest Guy"
+          color="brand.900"
+          fontSize={46}
+          mr="500px"
+          mt={4}
+        >
+          Chatropolis
+        </Heading>
+      </Link>
       <Box>
-        <Link to='/home'>Home</Link>
+        <Link to="/home">Home</Link>
       </Box>
       <Box>
-        <Link to='/room'>Room</Link>
+        <Link to="/room">Room</Link>
       </Box>
       <Box>
-        <Link to='private'>Private</Link>
+        <Link to="private">Private</Link>
       </Box>
     </Flex>
   );
