fe009d45a37e4c271ed1759d928b0cfa4bc16a04
Jennifer Techel <jennifertechel@hotmail.com>
2023-05-10 19:19:39 +0200

    style input field

--- client/src/components/MessageInput.tsx
+++ client/src/components/MessageInput.tsx
@@ -17,38 +17,39 @@ export default function MessageInput() {
 
   return (
     <>
-      <Flex align="center">
-        <Box w="285px" ml="0.5rem" border="1px">
-          <Form onSubmit={handleSubmit} id="messageInput">
-            <FormControl>
-              <Input
-                name="message"
-                placeholder="Write a message"
-                type="text"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                fontSize="0.8rem"
-                required
-              />
-            </FormControl>
-          </Form>
-        </Box>
-        <Box ml="0.4rem">
-          <Button
-            type="submit"
-            form="messageInput"
-            bg="none"
-            border="solid 1px"
-            borderRadius="none"
-            borderColor="brand.800"
-            color="brand.800"
-            fontWeight="medium"
-            fontSize="smaller"
-            _hover={{ bg: "brand.200", borderColor: "brand.200" }}
-          >
-            Send
-          </Button>
-        </Box>
+      <Flex align='center'>
+        <Form onSubmit={handleSubmit} id='messageInput'>
+          <FormControl>
+            <Input
+              name='message'
+              placeholder='Write a message'
+              type='text'
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              fontSize='smaller'
+              required
+              border='solid 1px '
+              borderRadius='none'
+              borderColor='brand.800'
+              w={64}
+            />
+          </FormControl>
+        </Form>
+        <Button
+          type='submit'
+          form='messageInput'
+          bg='none'
+          border='solid 1px'
+          borderRadius='none'
+          borderColor='brand.800'
+          color='brand.800'
+          fontWeight='medium'
+          fontSize='smaller'
+          ml={4}
+          _hover={{ bg: "brand.200", borderColor: "brand.200" }}
+        >
+          Send
+        </Button>
       </Flex>
     </>
   );
