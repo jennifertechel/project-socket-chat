8d72ac5a39e4e716d134572b98c7b4b7cbae385c
Martina Kreivi <martinakreivi@gmail.com>
2023-05-10 10:42:29 +0200

    Deleted private files

--- client/src/pages/PrivateChat.tsx
+++
@@ -1,5 +0,0 @@
-function PrivateChat() {
-  return <h1>This is private chat</h1>;
-}
-
-export default PrivateChat;
--- client/src/pages/PrivateNewPage.tsx
+++
@@ -1,5 +0,0 @@
-function PrivateNewPage() {
-  return <h1>This is private new page</h1>;
-}
-
-export default PrivateNewPage;
