4a9b3b1b232ed74255c7f49f37a1e770df98adaa
Martina Kreivi <martinakreivi@gmail.com>
2023-05-10 14:00:43 +0200

    (wip)

--- client/src/pages/RoomChat.tsx
+++ client/src/pages/RoomChat.tsx
@@ -1,34 +1,35 @@
 import { Box, Flex, Image, Text, useMediaQuery } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import Header from "../components/Header";
 import MessageBox from "../components/MessageBox";
 import MessageInput from "../components/MessageInput";
-import Header from "../components/Header";
-import { useParams } from "react-router-dom";
 
 function RoomChat() {
   const { roomId } = useParams();
   const [isMobile] = useMediaQuery("(max-width: 768px)");
 
+  // Lägg in bakåtknapp här och ta bort den från Header.tsx och Footer.tsx
   return (
     <Flex
-      flexDir='column'
-      justifyContent='center'
-      alignItems='center'
+      flexDir="column"
+      justifyContent="center"
+      alignItems="center"
       w={{ base: "95%", md: "50%" }}
       mt={{ base: 0, md: -36 }}
     >
       {!isMobile && <Header />}
-      <Box w='50%'>
-        <Image src='/assets/city.svg' />
+      <Box w="50%">
+        <Image src="/assets/city.svg" />
       </Box>
       <Box
-        bg='brand.900'
-        w='345px'
-        h='50px'
-        textAlign='center'
-        lineHeight='2.8rem'
-        mb='2rem'
+        bg="brand.900"
+        w="345px"
+        h="50px"
+        textAlign="center"
+        lineHeight="2.8rem"
+        mb="2rem"
       >
-        <Text fontFamily='Montserrat' color='white'>
+        <Text fontFamily="Montserrat" color="white">
           Tips in {roomId}
         </Text>
       </Box>
